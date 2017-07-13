24c88483bd9618fb115f47b8e5329a0bcec13150
thethatcher <thethatcher@gmail.com>
2017-07-13 13:32:01 -0600

    refactor to use objects instead of arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,56 +6,60 @@ var $timer = $('#timer');
 var $result = $('#result');
 var selectedAnswer;
 var questionCounter = 0;
-var questionArray = ["What year did RSL win the MLS cup?","Who is the all-time leading goal-scorer for RSL?"];
-var answerKey = [2,4];
-var answerArray = ["2008","2009","2012","2013","Yura Movsisian", "Kyle Beckerman", "Nick Rimando", "Alvaro Saborio"]
+var questionArray = [new question("How many MLS championships has RSL won?",1,0,2,3), 
+	new question("What year did RSL win the MLS Cup?",2009,2010,2012,2013),
+	new question("Who has scored the most goals for RSL all-time?","Alvaro Saborio","Jason Kries","Yura Movsisian","Nick Rimando"),
+	new question("What is the nick name of the stadium RSL plays in?","The RioT","New Delta Center","The Castle","Baseball Sucks"),
+	new question("Who is RSL's current head coach?","Mike Petke","Jason Kries","Kyle Beckerman","Jeff Cassar")]
 
-interval = setInterval(countdown,1000);
-$question.text("You will see the question here, pick one of the answers below.");
-for (var i = 0; i < $answers.length ; i++) {
-	$answers[i].text("answer " + (i+1));	
+function question(q,a1,a2,a3,a4){//a1 MUST be the correct answer!
+	this.question = q;
+	this.answers = [a1,a2,a3,a4];
+	this.correct = a1;
+	shuffleArray(this.answers);
+	function shuffleArray(array){
+		var j, x, i;
+	    for (i = array.length; i; i--) {
+	        j = Math.floor(Math.random() * i);
+	        x = array[i - 1];
+	        array[i - 1] = array[j];
+	        array[j] = x;
+    	}
+	}
 }
 
-function countdown(){
+$("button").click(function(){
+	$('.content').css("visibility","visible");
+	$('button').css('display','none');
+	nextQuestion();
+});
+
+$('.answer').click(function(){
+	
+	if (this === questionArray[questionCounter].correct) {
+		console.log("correct answer chosen");
+	}
+	else{
+		console.log("incorrect answer chosen");
+	}
+});
+
+function nextQuestion(){
+	interval = setInterval(timer,1000);
+	$question.text(questionArray[questionCounter].question);
+	for (var i = 0; i < $answers.length ; i++) {
+		$answers[i].text(questionArray[questionCounter].answers[i])
+	}
+}
+
+function timer(){
 	seconds--;
 	$timer.text(seconds);
-	if(seconds <=0){
+	if (seconds <= 0) {
 		timeUp();
 	}
 }
 
 function timeUp(){
 	clearInterval(interval);
-	seconds = 10;
-	$result.text("Time's Up!");
-	
-	if (checkAnswer()) {
-		$result.text("Correct!");
-	}
-	else{
-		$result.text($result.text() + " The correct answer is: " + answerArray[answerKey[questionCounter]-1]);
-	}
-	nextQuestion();
-}
-
-function checkAnswer(){
-	console.log("checking if " + selectedAnswer + " is correct");
-	console.log(selectedAnswer);
-	if(parseInt(answerKey[questionCounter]) === selectedAnswer){
-		return true;
-	}
-	else{return false;}
-}
-
-$('input:radio[name="answer"]').change(function(){
-	selectedAnswer = parseInt(this.value);
-	console.log("radio button " + selectedAnswer + " selected.");
-});
-
-function nextQuestion(){
-	$question.text(questionArray[questionCounter]);
-	for (var i = 0; i < 4; i++) {
-		$answers[i].text(answerArray[questionCounter*4 +i]);
-	}
-	questionCounter++;
 }
