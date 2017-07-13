c8e8708631c16db8efe3b6d68e11174b9837a292
thethatcher <thethatcher@gmail.com>
2017-07-13 15:25:00 -0600

    questions cycling appropriately, answers are clickable.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,8 +6,8 @@ var $timer = $('#timer');
 var $result = $('#result');
 var selectedAnswer;
 var questionCounter = 0;
-var questionArray = [new question("How many MLS championships has RSL won?",1,0,2,3), 
-	new question("What year did RSL win the MLS Cup?",2009,2010,2012,2013),
+var questionArray = [new question("How many MLS championships has RSL won?","1","0","2","3"), 
+	new question("What year did RSL win the MLS Cup?","2009","2010","2012","2013"),
 	new question("Who has scored the most goals for RSL all-time?","Alvaro Saborio","Jason Kries","Yura Movsisian","Nick Rimando"),
 	new question("What is the nick name of the stadium RSL plays in?","The RioT","New Delta Center","The Castle","Baseball Sucks"),
 	new question("Who is RSL's current head coach?","Mike Petke","Jason Kries","Kyle Beckerman","Jeff Cassar")]
@@ -35,31 +35,49 @@ $("button").click(function(){
 });
 
 $('.answer').click(function(){
-	
-	if (this === questionArray[questionCounter].correct) {
+	if ($(this).text() === questionArray[questionCounter].correct) {
 		console.log("correct answer chosen");
+		$result.text("Correct!")
 	}
 	else{
 		console.log("incorrect answer chosen");
+		$result.text("Wrong! The correct answer is " + questionArray[questionCounter].correct);
 	}
+	betweenQuestion();
 });
 
 function nextQuestion(){
+	console.log('nextQuestion called');
 	interval = setInterval(timer,1000);
 	$question.text(questionArray[questionCounter].question);
 	for (var i = 0; i < $answers.length ; i++) {
 		$answers[i].text(questionArray[questionCounter].answers[i])
 	}
+	seconds = 10;
+	$('#timerMsg').css("visibility","visible");
+	$timer.text(seconds);
+	$result.text("Answer the question!");
 }
 
 function timer(){
+	console.log('timer called');
 	seconds--;
 	$timer.text(seconds);
 	if (seconds <= 0) {
-		timeUp();
+		$result.text("Times Up! The correct answer is " + questionArray[questionCounter].correct);
+		betweenQuestion();
 	}
-}
+}	
 
-function timeUp(){
+function betweenQuestion(){
+	console.log('betweenQuestion called');
 	clearInterval(interval);
+	$('#timerMsg').css("visibility","hidden");
+	questionCounter++;
+	if(questionCounter < questionArray.length){
+		setTimeout(nextQuestion,3000);
+	}
+	else{
+		alert('game over! Thanks for playing!')
+	}
 }
